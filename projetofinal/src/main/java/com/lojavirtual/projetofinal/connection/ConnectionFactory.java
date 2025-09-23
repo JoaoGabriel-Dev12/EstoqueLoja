fc2a0057d46559e8946b8691c62da04e848da716
João Gabriel Eugênio Amorim <joaogabriel.dev12@gmail.com>
2025-09-22 23:59:00 -0300

    Update ConnectionFactory.java
    .

--- projetofinal/src/main/java/com/lojavirtual/projetofinal/connection/ConnectionFactory.java
+++ projetofinal/src/main/java/com/lojavirtual/projetofinal/connection/ConnectionFactory.java
@@ -5,9 +5,9 @@ import java.sql.DriverManager;
 
 public class ConnectionFactory {
 	
-	private static final String USERNAME = "root";
-	private static final String PASSWORD = "311084@Joao!";
-	private static final String DATABASE_URL = "jdbc:mysql://localhost:3306/estoque";
+	private static final String USERNAME = "";
+	private static final String PASSWORD = "";
+	private static final String DATABASE_URL = "";
 	
 	public Connection createConnectionMySql() throws Exception {
 		
