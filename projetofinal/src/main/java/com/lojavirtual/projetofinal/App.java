811273bbcac38136308d597e0e8600c5c14f7cad
João Gabriel Eugênio Amorim <joaogabriel.dev12@gmail.com>
2025-09-22 23:45:25 -0300

    Adicionado a fábrica de conexões (ConnectionFactory) com MySQL.

--- projetofinal/src/main/java/com/lojavirtual/projetofinal/App.java
+++ projetofinal/src/main/java/com/lojavirtual/projetofinal/App.java
@@ -1,10 +1,21 @@
 package com.lojavirtual.projetofinal;
 
+import java.sql.Connection;
+
+import com.lojavirtual.projetofinal.connection.ConnectionFactory;
+
 /**
  * Hello world!
  */
 public class App {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+    public static void main(String[] args) throws Exception {
+        
+    	ConnectionFactory connection = new ConnectionFactory();
+    	Connection con = connection.createConnectionMySql();
+    	
+    	if(con != null) {
+    		System.out.println("Conexão obtida com sucesso!");
+    		con.close();
+    	}
     }
 }
